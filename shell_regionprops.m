6ac9ed71f6f8814ffc349f356e5a079fec130d9e
RemcoPietersWUR <remco.pieters@wur.nl>
2017-09-07 10:36:42 +0200

    Shell_regionprops save pixel intensity values, post proc edit to load *_post_proc.mat

--- shell_regionprops.m
+++ shell_regionprops.m
@@ -1,11 +1,13 @@
 function shell_regionprops(BWfill,IMrot,px_z)
 %Calculate region properties
 %first for slice 1 to determine size table
-stats = regionprops('table',BWfill(:,:,1),IMrot(:,:,1),'Area','Perimeter');
+stats = regionprops('table',BWfill(:,:,1),IMrot(:,:,1),'Area','Perimeter','MinIntensity','MaxIntensity');
+density{1} = regionprops('struct',BWfill(:,:,1),IMrot(:,:,1),'PixelValues');
 Slice = ones(height(stats),1).*1;
 ShellProps = [table(Slice),stats];
 for id = 2:px_z
-stats = regionprops('table',BWfill(:,:,id),IMrot(:,:,id),'Area','Perimeter');
+stats = regionprops('table',BWfill(:,:,id),IMrot(:,:,id),'Area','Perimeter','MinIntensity','MaxIntensity');
+density{id} = regionprops('struct',BWfill(:,:,id),IMrot(:,:,id),'PixelValues');
 Slice = ones(height(stats),1).*id;
 ShellProps = [ShellProps;[table(Slice),stats]];
 end
@@ -15,4 +17,5 @@ if isequal(filenameProps,0) || isequal(pathnameProps,0)
    disp('User selected Cancel')
 else
    writetable(ShellProps,fullfile(pathnameProps,filenameProps));
+   save(fullfile(pathnameProps,'density.mat'),'density')
 end
